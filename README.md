43e201c760add2771b268498b45a2896fa189e60
Abd-Lah <hmamouchi.abdellah1@gmail.com>
2025-04-24 15:37:09 +0100

    Remove vendor, node_modules, and other ignored files from Git tracking

--- README.md
+++
@@ -1,2 +0,0 @@
-## [Single docs](https://github.com/Abd-Lah/bulk_emails_using_box/single) 
-## [Multiple docs](https://github.com/Abd-Lah/bulk_emails_using_box/multiple) 
--- single/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', \App\Livewire\EmailForm::class);
-Route::get('/email-content', \App\Livewire\ContentEditor::class)->name('email-content');
